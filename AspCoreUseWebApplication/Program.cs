81946327b141fb3bf5b9609be7eb5eefd9661da0
maxefimov <efimov.top.academy@gmail.com>
2024-03-10 14:14:00 +0300

    10-03-24

--- AspCoreUseWebApplication/Program.cs
+++ AspCoreUseWebApplication/Program.cs
@@ -1,3 +1,5 @@
+using static System.Net.Mime.MediaTypeNames;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -8,28 +10,32 @@ var app = builder.Build();
 //app.Use(async (context, next) =>
 //{
 //    date = DateTime.Now.ToShortDateString();
-//    await next.Invoke();  
+//    await next.Invoke();
 //    await context.Response.WriteAsync($"Date: {date}");
 //});
 
 //app.Run(async (context) => await context.Response.WriteAsync($"Current Date: {date}\n"));
 
 
-// 2
-//app.Use(async (context, next) =>
+//2
+//app.Use(UseOne);
+
+//app.Use(async (HttpContext context, RequestDelegate next) =>
 //{
-//    await context.Response.WriteAsync("<p>Hello world!</p>");
-//    await next.Invoke();
+//    await context.Response.WriteAsync("<p>Hello two!</p>");
+//    //await next.Invoke();
+//    await context.Response.WriteAsync("<p>Good by two!</p>");
 //});
+
 //app.Run(async (context) =>
 //{
-//    //await Task.Delay(10000);
-//    await context.Response.WriteAsync("<p>Good bye, World...</p>");
+//    await Task.Delay(5000);
+//    await context.Response.WriteAsync("<p>Hello abd Good by</p>");
 //});
 
 //3
 //app.UseWhen(
-//    context => context.Request.Path == "/time",
+//    (context) => context.Request.Path == "/time",
 //    appBuilder =>
 //    {
 //        appBuilder.Use(async (context, next) =>
@@ -53,23 +59,33 @@ var app = builder.Build();
 
 
 //4
-//app.UseWhen(
-//    context => context.Request.Path == "/time",
-//    appBuilder =>
-//    {
-//        appBuilder.Use(async (context, next) =>
-//        {
-//            var time = DateTime.Now.ToShortTimeString();
-//            Console.WriteLine($"Time: {time}");
-//            await next();
-//        });
-//    });
+app.UseWhen(
+    context => context.Request.Path == "/time",
+    appBuilder =>
+    {
+        appBuilder.Use(async (context, next) =>
+        {
+            var time = DateTime.Now.ToShortTimeString();
+            await context.Response.WriteAsync($"Time: {time}");
+            Console.WriteLine($"Time: {time}");
+            await next();
+        });
+    });
 
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Home page");
-//});
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Home page");
+});
 
 
 
 app.Run();
+
+
+
+async Task UseOne(HttpContext context, Func<Task> next)
+{
+    await context.Response.WriteAsync("<p>Hello one!</p>");
+    await next.Invoke();
+    await context.Response.WriteAsync("<p>Good one!</p>");
+}
